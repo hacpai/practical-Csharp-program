2861dce25b3dbf65732b88a4c859070db76e9362
arcticlion <david.malan@aol.com>
2014-12-17 14:23:12 +0800

    Add Drwa Ellipse program

---
+++ DrawEllipse/DrawEllipse/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DrawEllipse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawEllipse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drawEllipse
+            // 
+            this.drawEllipse.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.drawEllipse.Location = new System.Drawing.Point(481, 12);
+            this.drawEllipse.Name = "drawEllipse";
+            this.drawEllipse.Size = new System.Drawing.Size(75, 29);
+            this.drawEllipse.TabIndex = 0;
+            this.drawEllipse.Text = "Click";
+            this.drawEllipse.UseVisualStyleBackColor = true;
+            this.drawEllipse.Click += new System.EventHandler(this.drawEllipse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 522);
+            this.Controls.Add(this.drawEllipse);
+            this.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawEllipse;
+    }
+}
+
---
+++ DrawEllipse/DrawEllipse/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawEllipse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        Color getRandomColor ()
+        {
+            return Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        }
+        private void drawEllipse_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            int x0 = this.Width / 2;
+            int y0 = this.Height / 2;
+
+            for ( int r = 0; r < this.Width; r++ )
+            {
+                g.DrawEllipse( new Pen( getRandomColor(), 1),
+                    x0-r, y0-r, r*2, r*2 
+                    );
+            }
+            g.Dispose();
+        }
+    }
+}
