a82fd139b1f2e1fefce6144611b56069f05e1f85
arcticlion <david.malan@aol.com>
2014-12-20 15:58:15 +0800

    Explore Using

---
+++ ExploreUsing/ExploreUsing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MyClass obj = new MyClass())
+            {
+                Console.WriteLine("Using MyClass Object");
+            }
+        }
+    }
+
+    class MyClass:IDisposable
+    {
+        ~MyClass()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+
+
+        }
+    }
+}
