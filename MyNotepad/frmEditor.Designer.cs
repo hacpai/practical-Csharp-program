76aba3a23da4de5ec47a08c87c84f7cfaf1c28c7
arcticlion <david.malan@aol.com>
2014-12-21 00:52:32 +0800

    My Notepad

---
+++ MyNotepad/frmEditor.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MyNotepad
+{
+    partial class frmEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEditor));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(18, 18);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(82, 32);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(116, 20);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(73, 29);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(447, 266);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(53, 12);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "lblTimer";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(13, 266);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(41, 12);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "label1";
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEditor.Location = new System.Drawing.Point(15, 69);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(548, 180);
+            this.txtEditor.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // frmEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 287);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmEditor";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmEditor_FormClosing);
+            this.Load += new System.EventHandler(this.frmEditor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ MyNotepad/frmEditor.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNotepad
+{
+    public partial class frmEditor : Form
+    {
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 保存原始内容
+        /// </summary>
+        private String OriginalContent = "";
+        /// <summary>
+        /// 打开的文件名
+        /// </summary>
+        private String _FileName = "";
+        public String FileName
+        {
+            get
+            {
+                return _FileName;
+            }
+            set
+            {
+                _FileName = value;
+                //在赋值时同步更新窗体标题
+                Text = Path.GetFileName(value) + "-我的记事本";
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTimer.Text = DateTime.Now.ToString();
+        }
+
+        private void frmEditor_Load(object sender, EventArgs e)
+        {
+            lblTimer.Text = "";
+            lblInfo.Text = "";
+            Text = "无标题-我的记事本";
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            Open();
+
+        }
+
+        private void Open()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileName = openFileDialog1.FileName;
+                try
+                {
+                    OriginalContent = File.ReadAllText(FileName);
+                    txtEditor.Text = OriginalContent;
+                }
+                catch (Exception)
+                {
+                    lblInfo.Text = "无法打开文件";
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 保存到文件
+        /// </summary>
+        private void Save()
+        {
+            //当内容己更改时，此标记为True，
+            bool ShouldSave = false;
+            //如果文件名不为空，表明当前是文本框中的内容是来自于文件
+            if (FileName != "")
+            {
+                //如果内容己更改
+                if (txtEditor.Text != OriginalContent
+                    && MessageBox.Show("文件己修改，保存吗？",
+                    "保存文件",
+                    MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    ShouldSave = true;
+                }
+            }
+            else
+            {
+                //如果用户输入了内容，并且指定了一个文件名
+                if (txtEditor.Text != "" && saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FileName = saveFileDialog1.FileName;
+                    ShouldSave = true;
+                }
+            }
+
+            if (ShouldSave)
+            {
+                try
+                {
+                    File.WriteAllText(FileName, txtEditor.Text);
+                    OriginalContent = txtEditor.Text;
+                    lblInfo.Text = "文件己保存";
+                }
+                catch (Exception)
+                {
+                    lblInfo.Text = "文件保存失败";
+                }
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void frmEditor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Save();
+        }
+
+    }
+}
